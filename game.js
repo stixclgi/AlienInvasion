6fcc413bdc1994db37f23b23ac0d9568207b13bf
stixclgi <stixclgi@gmail.com>
2016-10-25 23:45:46 -0400

    Changing Title
    Changing Title - Training purposes

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ var startGame = function() {
     Game.setBoard(1,new Starfield(50,0.6,100));
     Game.setBoard(2,new Starfield(100,1.0,50));
   }  
-  Game.setBoard(3,new TitleScreen("Chris & Raj's Excellent Journey", 
+  Game.setBoard(3,new TitleScreen("Chris & Raj's Awesome Journey", 
                                   "Press fire to start playing",
                                   playGame));
 };
